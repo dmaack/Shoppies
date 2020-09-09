29540b59fdf41e42b9ffe2dc298b9aa239d3c71e
dmaack <dominiquemaack@gmail.com>
2020-09-09 16:00:19 -0600

    changed background style

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
+import loader from './loading-78.gif';
 
 
 const Spinner = () => {
     return (
         <Fragment>
-            <img src={spinner} alt='Loading...' style={{width: '200px', margin: 'auto', display: 'block'}} />
+            <img src={loader} alt='Loading...' style={{width: '200px', margin: 'auto', display: 'block', backgroundColor:'rgba(36, 36, 37, 0.9);'}} />
         </Fragment>
     )
 };
